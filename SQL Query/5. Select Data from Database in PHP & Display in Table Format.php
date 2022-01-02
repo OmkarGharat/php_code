f823bdd7c2f4304acd904dae43c9894a98dcc6fa
Omkar <omkargharat578@gmail.com>
2022-01-03 00:10:58 +0530

    File uploaded on 03/01/2022 at 12:10 am

---
+++ SQL Query/5. Select Data from Database in PHP & Display in Table Format.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style>
+        .box {
+            margin: 2rem 0.5rem;
+            padding: 1rem 0 1rem 1rem;
+            color: red;
+            box-shadow: 0 0 8px rgb(0 0 0 / 30%);
+            text-align: center;
+        }
+
+        .table {
+            box-shadow: 0 0 1rem -0.09rem rgb(10 10 10 / 28%), 0 0 0 1px rgb(10 10 10 / 2%);
+            border-radius: 3px;
+        }
+
+        th,
+        td {
+            text-align: center;
+            border: none;
+            border-right: 2px solid #898989 !important;
+            border-left: 2px solid #898989 !important;
+        }
+        
+
+        .row_header {
+            font-size: 1.5rem;
+            border-bottom: 2px solid #898989 !important;
+            border-left: 2px solid #898989 !important;
+            background-color: #fbffc3 !important;
+            color: #ff651e ;
+        }
+    </style>
+</head>
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "Mydb1";
+
+$connection = mysqli_connect($servername, $username, $password, $database);
+
+//    if ($connection){
+//        echo "Connection is successful";
+//    }else{
+//        die("Connection failed" .mysqli_connect_error());
+//    }
+
+
+?>
+
+<body>
+    <div class="container">
+        <div class="box">
+            <h1>Select Data from Database in PHP & Display in Table Format</h1>
+        </div>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="row_header" scope="col">Sr No.</th>
+                    <th class="row_header" scope="col">Name</th>
+                    <th class="row_header" scope="col">Email</th>
+                    <th class="row_header" scope="col">Password</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+
+                $select_query = "SELECT * FROM `form_details` ";
+                $result = mysqli_query($connection, $select_query);
+
+                // if($result){
+                //     echo " We are ready to display records from database";
+                // }
+
+                $nums = mysqli_num_rows($result);
+                // echo "$nums";
+
+                $srNo = 0;
+
+                while ($res = mysqli_fetch_array($result)) {
+                    $srNo++;
+                    echo "<tr>
+                <th scope='row'>" . $srNo . "</th>
+                <td>" . $res['Name'] . "</td>
+                <td>" . $res['Email'] . "</td>
+                <td>" . $res['Password'] . "</td>
+            </tr> ";
+                }
+
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
